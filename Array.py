663abbfb3119a729e7b3b9cee2c03753b9a5af56
iknoor12 <imiknoor@gmail.com>
2025-11-15 11:10:46 +0530

    Solve basic, medium array questions

---
+++ Array.py
@@ -0,0 +1,30 @@
+# Easy
+# Given an integer array nums, return True if any value appears at least twice in the array, and return False.
+# Input: nums = [1, 2, 3, 1],  Output: True
+
+def containsDuplicate(nums):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+
+nums = [1, 2, 3, 1]
+print(containsDuplicate(nums))
+
+
+# Medium
+# Given an array nums, rotate the array to the right by k steps.
+# Input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3,  Output: [5, 6, 7, 1, 2, 3, 4]
+
+def rotate(nums, k):
+    n = len(nums)
+    for i in range(k):
+        last  = nums[n-1]
+        for j in range(n-1, 0, -1):
+            nums[j] = nums[j-1]
+        nums[0] = last
+    return nums
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3 
+print(rotate(nums, k))
